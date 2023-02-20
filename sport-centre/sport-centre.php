e65dfab9f79646b0edab799710d741927f67d153
Angelique-Smit <ajd.smit@gmail.com>
2023-02-20 13:56:58 +0100

    p

--- sport-centre/sport-centre.php
+++ sport-centre/sport-centre.php
@@ -1,1 +1,58 @@
-<?php
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    </nav>
+
+    <header>
+
+    </header>
+
+    <main>
+        <section class="slides">
+            <div class="slide">
+                <img alt="Soccer" src="">
+                <h2> Soccer </h2>
+            </div>
+
+            <div class="slide">
+                <img alt="Fitness" src="">
+                <h2> Fitness </h2>
+            </div>
+
+            <div class="slide">
+                <img alt="Bowling" src="">
+                <h2> Bowling </h2>
+            </div>
+
+            <div class="slide">
+                <img alt="tennis" src="">
+                <h2> Tennis </h2>
+            </div>
+
+            <div class="slide">
+                <img alt="Basketball" src="">
+                <h2> Basketball </h2>
+            </div>
+
+            <div class="slide">
+                <img alt="Swimming" src="">
+                <h2> Swimming </h2>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
