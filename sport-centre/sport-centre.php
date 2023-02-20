9ea99aebc19c205c536ddd28fbc66a33a3f44840
Angelique-Smit <ajd.smit@gmail.com>
2023-02-20 15:11:56 +0100

    p

--- sport-centre/sport-centre.php
+++ sport-centre/sport-centre.php
@@ -7,7 +7,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>sport-centre</title>
+    <link rel="stylesheet" type="text/css" href="css-sport-centre/sport-centre.css">
+
 </head>
 <body>
     <nav>
@@ -20,33 +22,40 @@
     <main>
         <section class="slides">
             <div class="slide">
-                <img alt="Soccer" src="">
-                <h2> Soccer </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="Soccer" src="https://media.s-bol.com/xn2QnjERnyO9/550x544.jpg">
+                 Voetbal</a>
             </div>
 
             <div class="slide">
-                <img alt="Fitness" src="">
-                <h2> Fitness </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="Fitness" src="photo-sport-centre/dumbbell_PNG16378.png">
+                Fitness</a>
+
             </div>
 
             <div class="slide">
-                <img alt="Bowling" src="">
-                <h2> Bowling </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="Bowling" src="photo-sport-centre/bowling-pin.png">
+                Bowlen</a>
             </div>
 
             <div class="slide">
-                <img alt="tennis" src="">
-                <h2> Tennis </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="tennis" src="photo-sport-centre/tennis-racket.jpg">
+                Tennis</a>
             </div>
 
             <div class="slide">
-                <img alt="Basketball" src="">
-                <h2> Basketball </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="Basketball" src="photo-sport-centre/basketball.png">
+                Basketbal</a>
             </div>
 
             <div class="slide">
-                <img alt="Swimming" src="">
-                <h2> Swimming </h2>
+                <a class="slide-link" href="soccer.php">
+                <img class="slide-img" alt="Swimming" src="photo-sport-centre/swimming.png">
+                Zwemmen</a>
             </div>
         </section>
     </main>
