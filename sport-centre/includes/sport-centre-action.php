8ecced0d53c470826f45b25db7a08bcb0c520325
Angelique-Smit <ajd.smit@gmail.com>
2023-03-22 12:37:39 +0100

    Added a javascript file and some dom but the fetch doesnt work yet so please ignore it. I also added in my json data. There are some slight errors with it but just ignore it for now <3

---
+++ sport-centre/includes/sport-centre-action.php
@@ -0,0 +1,101 @@
+<?php
+
+function getBuilding () {
+    return [
+        [
+            "id" => 1,
+            "name" => "sportchool 1",
+            "location" => "locatie 1",
+            "sport" => ["footbal", "basketbal", "tennis", "swimming"],
+            "enterSpecs" => ["helling"],
+            "buildingSpecs" => ["lift"],
+            "doors" => ["Automatisch"],
+        ],
+
+        [
+            "id" => 2,
+            "name" => "Sportschool 2",
+            "sport" => ["footbal", "basketbal", "tennis", "bowling", "fitness"],
+            "location" => "Blaak",
+            "enterSpecs" => ["rolstoelLift"],
+            "buildingSpecs" => ["Lift", "invalideToilet", "ruimte"],
+            "doors" => ["knop"]
+        ],
+
+        [
+            "id" => 3,
+            "name" => "Sportschool 3",
+            "sport" => ["footbal", "fitness"],
+            "location" => "Beurs",
+            "enterSpecs" => ["Helling"],
+            "buildingSpecs" => [ "Lift", "Invalide toilet"],
+            "doors" => ["Automatisch"]
+        ],
+
+        [
+            "id" => 4,
+            "name" => "Sportschool 4",
+            "sport" => ["bowling"],
+            "location" => "Beurs",
+            "enterSpecs" => [""],
+            "buildingSpecs" => ["ruimte"],
+            "doors" => ["Automatisch"],
+        ],
+
+        [
+            "id" => 5,
+            "name" => "Sportschool 5",
+            "sport" => ["footbal", "basketbal", "tennis", "bowling", "fitness", "swimming"],
+            "location" => "Slinge",
+            "enterSpecs" => [""],
+            "buildingSpecs" => ["Invalide toilet"],
+            "doors" => [""],
+        ],
+
+        [
+            "id" => 6,
+            "name" => "sportchool 1",
+            "location" => "Kralingsezoom",
+            "enterSpecs" => ["helling"],
+            "buildingSpecs" => ["lift"],
+            "doors" => ["Automatisch"],
+        ],
+
+        [
+            "id" => 7,
+            "name" => "",
+            "location" => "Eendrachtsplein",
+            "enterSpecs" => ["rolstoelLift"],
+            "buildingSpecs" => ["Lift", "invalideToilet", "ruimte"],
+            "doors" => ["knop"]
+        ],
+
+        [
+            "id" => 8,
+            "name" => "Sportschool 3",
+            "location" => "Schiedam",
+            "enterSpecs" => ["Helling"],
+            "buildingSpecs" => [ "Lift", "Invalide toilet"],
+            "doors" => ["Automatisch"]
+        ],
+
+        [
+            "id" => 9,
+            "name" => "Sportschool 4",
+            "location" => "Alexander",
+            "enterSpecs" => [""],
+            "buildingSpecs" => ["ruimte"],
+            "doors" => ["Automatisch"],
+        ],
+
+        [
+            "id" => 10,
+            "name" => "Sportschool 5",
+            "location" => "Slinge",
+            "enterSpecs" => [""],
+            "buildingSpecs" => ["Invalide toilet"],
+            "doors" => [""],
+        ],
+    ];
+}
+
