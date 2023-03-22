5a902ca9ca462c4021d2fc9920b3c4b5de240a9e
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2023-03-22 14:11:18 +0100

    html bestanden in php gezet, actions.php en index.php gemaakt. verder gaan met fetch command :3

--- sport-equipment/core.php
+++ sport-equipment/core.php
@@ -1,18 +1,3 @@
 <?php
 require_once "includes/actions.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Core</title>
-</head>
-<body>
-<a href="home.html">Terug</a>
-<h1>Core</h1>
-<p>Hier komt informatie over het geselecteerde lichaamsdeel en hoe je het kunt trainen</p>
-<br>
-<p>Hier komt alle JSON data van producten die met het geselecteerde lichaamsdeel
-    te maken hebben.</p>
-</body>
-</html>
