c33aa94f879f7270167616b9dee8ae544022ee7c
AnnyXi <1061555@hr.nl>
2023-03-27 15:49:16 +0200

    Test github

--- sport-type/index.php
+++ sport-type/index.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="js/main.js" defer></script>
-    <title>Sport typezsss</title>
+    <title>Sport type</title>
 </head>
 <body>
 <main id="main_sport_type">
