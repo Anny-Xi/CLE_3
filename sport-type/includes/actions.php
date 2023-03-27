76f80a69f1e4516ce54565560765e1eaa24f631a
AnnyXi <1061555@hr.nl>
2023-03-27 13:35:43 +0200

    ANNY : add file structure, js, css, img

--- sport-type/includes/actions.php
+++ sport-type/includes/actions.php
@@ -1,64 +1,181 @@
 <?php
-/**
+/**getSport
  * @return array
  */
-function getDishes()
+function getSport()
 {
     return [
+        "naam van server" => "Sport type",
         [
-            "id" => 1,
-            "name" => "Pizza",
-            "kitchen" => "Italian",
-        ],
-        [
-            "id" => 2,
-            "name" => "Kale",
-            "kitchen" => "Dutch",
-        ],
-        [
-            "id" => 3,
-            "name" => "Lasagna",
-            "kitchen" => "Italian",
-        ],
-        [
-            "id" => 4,
-            "name" => "Kebab",
-            "kitchen" => "Turkish",
-        ],
-        [
-            "id" => 5,
-            "name" => "Paella",
-            "kitchen" => "Spanish",
+            "Tag" => [
+                [ "tagNaam" => "beperking",
+                    [
+                        "naam" => "armen",
+                        [
+                            "id" => 1,
+                            "niveau" => "licht beperkt"
+                        ],
+                        [
+                            "id" => 2,
+                            "niveau" => "beperkt "
+                        ],
+                        [
+                            "id" => 3,
+                            "niveau" => "erg beperkt"
+                        ]
+                    ],
+                    [
+                        "naam" => "benen",
+                        [
+                            "id" => 1,
+                            "niveau" => "licht beperkt"
+                        ],
+                        [
+                            "id" => 2,
+                            "niveau" => "beperkt "
+                        ],
+                        [
+                            "id" => 3,
+                            "niveau" => "erg beperkt"
+                        ]
+                    ]
+                ],
+                [
+                    "tagNaam" => "locatie",
+                    [
+                        "id" => 1,
+                        "naam" => "binnen"
+                    ],
+                    [
+                        "id" => 2,
+                        "naam" => "buiten "
+                    ],
+                    [
+                        "id" => 3,
+                        "naam" => "binnen en buiten"
+                    ]
+                ],
+                [
+                    "tagNaam" => "kenmerk",
+                    [
+                        "id" => 1,
+                        "naam" => "binnen"
+                    ],
+                    [
+                        "id" => 2,
+                        "naam" => "buiten "
+                    ],
+                    [
+                        "id" => 3,
+                        "naam" => "binnen en buiten"
+                    ]
+                ]
+            ],
+            "Sport" => [
+                [
+                    "id" => 1,
+                    "naam" => "zwemmen",
+                    "beschrijving" => "beschrijving zwemmen",
+                    "tag" => ["1", "bennen"]
+                ],
+                [
+                    "id" => 2,
+                    "naam" => "gewichtheffen",
+                    "beschrijving" => "beschrijving gewichtheffen",
+                    "impairment" => ["bennen"]
+                ],
+                [
+                    "id" => 3,
+                    "naam" => "boogschieten",
+                    "beschrijving" => "beschrijving boogschieten",
+                    "impairment" => ["bennen"]
+                ],
+                [
+                    "id" => 4,
+                    "naam" => "fietsen",
+                    "beschrijving" => "beschrijving fietsen",
+                    "impairment" => ["bennen", "armen"]
+                ],
+                [
+                    "id" => 5,
+                    "naam" => "kano",
+                    "beschrijving" => "beschrijving kano",
+                    "impairment" => ["bennen"]
+                ],
+                [
+                    "id" => 6,
+                    "naam" => "skiën",
+                    "beschrijving" => "beschrijving skiën",
+                    "impairment" => ["bennen", "arm"]
+                ]
+            ]
         ]
     ];
+//    return [
+//        [
+//            "id" => 1,
+//            "name" => "zwemmen",
+//            "impairment" => ["armen","bennen"]
+//        ],
+//        [
+//            "id" => 2,
+//            "name" => "gewichtheffen",
+//            "impairment" => ["bennen"]
+//        ],
+//        [
+//            "id" => 3,
+//            "name" => "boogschieten",
+//            "impairment" => ["bennen"]
+//        ],
+//        [
+//            "id" => 4,
+//            "name" => "fietsen",
+//            "impairment" => ["bennen", "armen"]
+//        ],
+//        [
+//            "id" => 5,
+//            "name" => "kano",
+//            "impairment" => ["bennen"]
+//        ],
+//        [
+//            "id" => 6,
+//            "name" => "skiën",
+//            "impairment" => ["bennen", "arm"],
+//            "location" => ["sneeuw"]
+//        ]
+//    ];
 }
 
 /**
  * @param $id
  * @return mixed
  */
-function getDishDetails($id)
+function getSportDetail($id)
 {
     $tags = [
         1 => [
-            "recipe" => "Put it in the oven and go!",
-            "tags" => ['cheese', 'oven']
+            "beschrijving" => "beschrijving zwemmen",
+            "tags" => ['water', 'binnen en buiten']
         ],
         2 => [
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+            "beschrijving" => "beschrijving gewichtheffen",
+            "tags" => ['droog', 'binnen']
         ],
         3 => [
-            "recipe" => "Very nice when your grandma prepares this meal",
-            "tags" => ['omnomnom']
+            "beschrijving" => "beschrijving boogschieten",
+            "tags" => ['droog', 'binnen en buiten']
         ],
         4 => [
-            "recipe" => "Everytime in the city after midnight",
-            "tags" => ['kapsalon', 'tasty', 'meat']
+            "beschrijving" => "beschrijving fietsen",
+            "tags" => ['droog', 'binnen en buiten']
         ],
         5 => [
-            "recipe" => "Specialty when on holiday in Spain",
-            "tags" => ['fish']
+            "beschrijving" => "beschrijving kano",
+            "tags" => ['water', 'buiten']
+        ],
+        6 => [
+            "beschrijving" => "beschrijving skiën",
+            "tags" => ['sneeuw', 'binnen en buiten']
         ],
     ];
 
