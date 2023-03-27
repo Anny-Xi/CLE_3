1c1ca5052d8dbaa3feb5b3c762237926e803b175
AnnyXi <1061555@hr.nl>
2023-03-27 14:56:39 +0200

    testing push

--- sport-type/index.php
+++ sport-type/index.php
@@ -1,21 +1,23 @@
-<?php
-//Require functions for actions
-require_once "includes/actions.php";
 
-$data = getSport();
-
-
-//Based on the existence of the GET parameter, 1 of the 2 functions will be called
-//if (!isset($_GET['id'])) {
-//    $data = getSport();
-//} else {
-//    $data = getSportDetail($_GET['id']);
-//}
-
-//Set the header & output JSON so the client will know what to expect.
-header("Content-Type: application/json");
-
-//get encode the data from getSport function
-$dataArray = json_encode($data);
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="js/main.js" defer></script>
+    <title>Sport typezsss</title>
+</head>
+<body>
+<main id="main_sport_type">
+    <section id="formField">
+        <div id="button_open_form">
+            <h2>Maak een test</h2>
+            <p id="form_description">Kijk welke sport (misschien) bij u past</p>
+        </div>
+    </section>
+</main>
+</body>
+</html>
 
--- sport-type/js/main.js
+++ sport-type/js/main.js
@@ -1,9 +1,86 @@
 window.addEventListener('load', init);
 
+//Globals
+let apiUrl = '../includes/action.php'
+// let galary
+
 /**
  * Initialize after the DOM is ready
  */
 function init()
 {
+    galary = document.getElementById("pokemon-gallery")
+    getSport()
+}
 
+/**
+ * Do the actual AJAX call to the provided URL
+ */
+function getSport() {
+    fetch(apiUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json()
+        })
+        .then(getPokemonsSuccessHandler)
+        .catch(getPokemonsErrorHandler)
+}
+
+/**
+ * Do something nice with the data you got from the external API
+ *
+ * @param data
+ */
+function getPokemonsSuccessHandler(data) {
+    console.log(data);
+    for (const pokemon of data.results){
+        console.log(pokemon)
+        let div = document.createElement('div')
+        div.classList.add('pokemon-card')
+        //!!
+        div.dataset.name = pokemon.name
+
+        galary.appendChild(div)
+
+        fetch(pokemon.url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json()
+            })
+            .then(getPokemonDetailSuccessHandler)
+    }
+}
+
+function getPokemonDetailSuccessHandler(pokemon) {
+    console.log(pokemon)
+    console.log(pokemon.name, "#", pokemon.id)
+    console.log(pokemon.sprites.other.home.front_default)
+
+    //!!!!!!!
+    let card = document.querySelector(`.pokemon-card[data-name ="${pokemon.name}"]`)
+    //!!!
+
+    let h2 = document.createElement('h2')
+    h2.innerText = `${pokemon.name} (#${pokemon.id})`
+    card.appendChild(h2)
+
+    let img = document.createElement('img')
+    img.src = pokemon.sprites.other.home.front_default
+    card.appendChild(img)
+}
+
+/**
+ * Do something useful with the error you got back from the external API
+ *
+ * @param data
+ */
+function getPokemonsErrorHandler(data) {
+    console.log(data);
+    let error = document.createElement('div')
+    error.innerHTML = "Er gaat iets fout!"
+    galary.before(error)
 }
