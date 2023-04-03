b7947acbd1565fb14594f5f207dfe1fb8e1f9c7f
AnnyXi <1061555@hr.nl>
2023-04-03 19:29:30 +0200

    ANNY: Fix js function for fetch

--- sport-type/index.php
+++ sport-type/index.php
@@ -6,18 +6,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="js/main.js" defer></script>
+    <link rel="stylesheet" href="css/style.css">
     <title>Sport type</title>
 </head>
 <body>
 <main id="main_sport_type">
     <h1 class="main-title">Sporten voor mensen met beperkingen</h1>
+
+    <!--    section for show form page-->
     <section id="formField">
-        <div id="button_open_form">
+        <div id="open_form">
             <h2>Maak een test</h2>
             <p id="form_description">Kijk welke sport (misschien) bij u past</p>
         </div>
     </section>
+    <!--    Section for a list with all sports-->
     <section id="Sport_List"></section>
+
+    <dialog class="modal" id="sport-detail">
+        <div class="modal-content"></div>
+        <button class="modal-close">❌</button>
+    </dialog>
+
+
 </main>
 </body>
 </html>
--- sport-type/js/main.js
+++ sport-type/js/main.js
@@ -2,21 +2,32 @@ window.addEventListener('load', init);
 
 //Globals
 let apiUrl = "sport_types.php"
+let sportData = {}
 
 let mainPage
 let sportContainer
-
+let detailDialog;
+let detailContent;
 
 
 /**
  * Initialize after the DOM is ready
  */
-function init()
-{
+function init() {
     mainPage = document.getElementById("main_sport_type")
+    mainPage.addEventListener('click',sportClickHandler)
 
     sportContainer = document.getElementById("Sport_List")
 
+
+    //Retrieve modal elements, and add click event for closing modal
+    detailDialog = document.getElementById('sport-detail');
+    detailContent = detailDialog.querySelector('.modal-content');
+    detailDialog.addEventListener('click', detailModalClickHandler);
+    detailDialog.addEventListener('close', dialogCloseHandler);
+
+
+    //Start the application with loading the API data
     fetchApi(apiUrl, getSport)
 
 }
@@ -43,63 +54,91 @@ function fetchApi(apiUrl, successHandler) {
  */
 function getSport(data) {
 
-    // console.log(data)
-
     //Loop through the list of sport
     for (let sport of data.Sport) {
 
         let sportCard = document.createElement('div');
-        sportCard.classList.add('pokemon-card');
+        sportCard.classList.add('sport-card');
         sportCard.dataset.name = sport.naam;
 
-        //Append Sport card to the actual HTML
-        sportContainer.appendChild(sportCard);
+        //Append sport card to the actual HTML -> Main sport list
+        mainPage.appendChild(sportCard)
 
         let sportUrl = `sport_types.php?id=${sport.id}`
-        console.log(sportUrl)
 
         //Retrieve the detail information from the API
-        fetchApi(sportUrl, getSportSuccessHandler)
+        fetchApi(sportUrl, fillSportCard)
     }
 }
 
 /**
- * Do something nice with the data you got from the external API
+ * Create the actual contents of the card based on the loaded API information
  *
- * @param data
+ * @param sport
  */
-function getSportSuccessHandler(data) {
-    console.log(data)
-    let sport = data
+function fillSportCard(sport) {
 
-    console.log(sport)
     console.log(sport.naam)
 
-    let div = document.createElement('div')
+    //Wrapper element for every sport card
+    let sportCard = document.querySelector(`.pokemon-card[data-name='${sport.naam}']`);
 
-    let h2 = document.createElement('h2')
-    h2.innerText = `${pokemon.name} (#${pokemon.id})`
-    div.appendChild(h2)
+    //Element for the name of the Pokémon
+    let title = document.createElement('h2');
+    title.innerHTML = `${sport.id}`;
+    // sportCard.appendChild(title);
 
+    // //Element for the image of the Pokémon
+    // let image = document.createElement('img');
+    // image.src = pokemon.sprites.other.home.front_default;
+    // sportCard.appendChild(image);
+
+    //Element for the button to load detail about that pokemon
+    let button = document.createElement('button');
+    button.innerHTML = 'Show Detail';
+    button.dataset.id = sport.id;
+
+    // sportCard.appendChild(button);
+
+    //Store Pokémon data globally for later use in other functions
+    // pokemonData[sport.id] = sport;
 }
 
-function getSportDetailSuccessHandler(sport) {
-    console.log(pokemon)
-    console.log(pokemon.name, "#", pokemon.id)
-    console.log(pokemon.sprites.other.home.front_default)
+/**
+ * Open the detailview with information of a Pokémon
+ *
+ * @param e
+ */
+function sportClickHandler(e)
+{
+    let clickedItem = e.target;
 
-    //!!!!!!!
-    let card = document.querySelector(`.pokemon-card[data-name ="${pokemon.name}"]`)
-    //!!!
+    //Check if we clicked on a button
+    if (clickedItem.nodeName !== 'BUTTON') {
+        return;
+    }
 
-    let h2 = document.createElement('h2')
-    h2.innerText = `${pokemon.name} (#${pokemon.id})`
-    card.appendChild(h2)
+    //Get the information from the global stored data
+    let pokemon = pokemonData[clickedItem.dataset.id];
 
-    let img = document.createElement('img')
-    img.src = pokemon.sprites.other.home.front_default
-    card.appendChild(img)
+    //Reset the content
+    detailContent.innerHTML = '';
+
+    //Show the name we used on the main grid
+    let title = document.createElement('h1');
+    title.innerHTML = `${pokemon.name} (#${pokemon.id})`;
+    detailContent.appendChild(title);
+
+    //Display the shiny
+    let shiny = document.createElement('img');
+    shiny.src = pokemon.sprites.other.home.front_shiny;
+    detailContent.appendChild(shiny);
+
+    //Open the modal
+    detailDialog.showModal();
+    gallery.classList.add('dialog-open');
 }
+
 
 /**
  * Do something useful with the error you got back from the API
@@ -111,4 +150,26 @@ function getSportErrorHandler(data) {
     let error = document.createElement('div')
     error.innerHTML = "Er gaat iets fout!"
     mainPage.before(error)
+}
+
+/**
+ * Close the modal if clicked on the close button or outside the modal (on the backdrop)
+ *
+ * @param e
+ */
+function detailModalClickHandler(e)
+{
+    if (e.target.nodeName === 'DIALOG' || e.target.nodeName === 'BUTTON') {
+        detailDialog.close();
+    }
+}
+
+/**
+ * Close the underlying blur effect when dialog is closed (both on our own click or the native ESC key)
+ *
+ * @param e
+ */
+function dialogCloseHandler(e)
+{
+    gallery.classList.remove('dialog-open');
 }
