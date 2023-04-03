d56fe1f878ceaa8be678fa93ba7c535762373615
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2023-04-03 14:00:46 +0200

    form.js verplaats in main.js :3

--- sport-equipment/js-sport-equipment/main.js
+++ sport-equipment/js-sport-equipment/main.js
@@ -6,6 +6,9 @@ let limbData = {};
 let detailDialogue;
 let favoriteItem;
 let detailContent;
+let todoItems = [];
+let inputField;
+let list;
 
 
 /**
@@ -24,11 +27,28 @@ function init()
     detailDialogue = document.getElementById('limb-detail');
     detailContent = document.querySelector('.modal-content');
     detailDialogue.addEventListener('click', detailModalClickHandler);
-    detailDialogue.addEventListener('close', dialogCloseHandler);
 
     favoriteItem = document.getElementById('favorite');
     favoriteItem.addEventListener('click', addFavoriteItem);
     favoriteItem.addEventListener('click', favoriteClickHandler);
+
+    //Connect variables with HTML elements
+    let form = document.querySelector('#favorite-form');
+    inputField = document.querySelector('#favorite-field');
+    list = document.querySelector('#list');
+
+    //Add event listeners for form & removal
+    form.addEventListener('submit', formSubmitHandler);
+    list.addEventListener('click', todoItemClickHandler);
+
+    //Retrieve current items from local storage & add them to the list
+    let todoItemsString = localStorage.getItem('todoItems');
+    if (todoItemsString) { //Or: if (todoItemsString !== null) {
+        todoItems = JSON.parse(todoItemsString);
+        for (let todoItem of todoItems) {
+            addTodoItem(todoItem);
+        }
+    }
 
 
 
@@ -90,6 +110,8 @@ function limbClickHandler(e){
 function getLimbDetail(limb){
     let limbCard = document.querySelector(`.modal-content`);
 
+    limbCard.innerHTML = '';
+
     let title = document.createElement('h2');
     title.innerHTML = `${limb.naam}  (#${limb.tags})`;
     limbCard.appendChild(title);
@@ -129,9 +151,60 @@ function favoriteClickHandler(e){
 function addFavoriteItem(e){
 
 }
-
-
-function dialogCloseHandler(e)
+function formSubmitHandler(e)
 {
-    limbs.classList.remove('dialog-open');
+    e.preventDefault();
+
+    //Check if the field is not empty
+    let inputValue = inputField.value;
+    if (inputValue !== '') {
+        //Add to the HTML list & local storage
+        addTodoItem(inputValue);
+        todoItems.push(inputValue);
+        localStorage.setItem('todoItems', JSON.stringify(todoItems));
+
+        //Reset the field
+        inputField.value = '';
+        inputField.classList.remove('error');
+    } else {
+        //Add an error state with CSS
+        inputField.classList.add('error');
+    }
 }
+
+/**
+ * Add a new item to the HTML
+ *
+ * @param todoText
+ */
+function addTodoItem(todoText)
+{
+    let listItem = document.createElement('li');
+    listItem.innerText = todoText;
+    list.appendChild(listItem);
+}
+
+/**
+ * Remove the clicked list item
+ *
+ * @param e
+ */
+function todoItemClickHandler(e)
+{
+    let todoTarget = e.target;
+
+    //Only continue if we clicked on a list item
+    if (todoTarget.nodeName !== 'LI') {
+        return;
+    }
+
+    //Remove from local storage
+    let itemIndex = todoItems.indexOf(todoTarget.innerText);
+    todoItems.splice(itemIndex, 1);
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+
+    //Remove from HTML
+    todoTarget.remove();
+}
+
+
