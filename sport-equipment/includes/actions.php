27e2215c5f15b452ba67bee04043e7f7130c1b90
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2023-04-04 10:54:30 +0200

    json data aangepast met echte informatie

--- sport-equipment/includes/actions.php
+++ sport-equipment/includes/actions.php
@@ -41,19 +41,23 @@ function getApparatuurDetails($id)
             "tags" => ['polsen', 'onderarm']
         ],
         2 => [
-            "informatie" => "test",
+            "informatie" => "Met verstelbare dumbbells kun je het gewicht van de dumbbell veranderen, wat handig kan zijn als je beperkte mobiliteit of kracht hebt. 
+            Sommige verstelbare dumbbells kunnen met slechts één hand worden aangepast, waardoor ze gemakkelijker te gebruiken zijn",
             "naam" => "Dumbbells",
             "tags" => ['onderarm', 'opperarm']
         ],
         3 => [
-            "informatie" => "test",
-            "naam" => "test4",
-            "tags" => ['buik', 'heup']
+            "informatie" => "Onderarm-oefenballen zijn kleine, 
+            in de hand te houden ballen die zijn ontworpen om de grijpkracht en het 
+            uithoudingsvermogen van de onderarmspieren te verbeteren.",
+            "naam" => "Onderarm oefenbal",
+            "tags" => ['onderarm', 'pols']
         ],
         4 => [
-            "informatie" => "test",
-            "naam" => "test5",
-            "tags" => ['buik', 'opperarm']
+            "informatie" => "Handgrijpers zijn kleine apparaten die u met uw vingers samenknijpt om kracht in uw hand en vingers op te bouwen. 
+            Ze zijn er in verschillende weerstandsniveaus en zijn een geweldige manier om je grijpkracht te verbeteren.",
+            "naam" => "Hand grijpers",
+            "tags" => ['vingers', 'polsen']
         ],
     ];
 
